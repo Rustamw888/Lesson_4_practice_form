0f65ae60ef255f7ef79addba688591acce95c4de
QAmilord <i@qamilord.ru>
2022-04-03 03:05:26 +0300

    added annotations test examples

--- src/test/java/quru/qa/SimpleTest.java
+++ src/test/java/quru/qa/SimpleTest.java
@@ -1,4 +1,43 @@
 package quru.qa;
 
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.*;
+
 public class SimpleTest {
+
+    @BeforeAll
+    static void initDB() {
+        System.out.println("### BeforeAll");
+    }
+
+    @BeforeEach
+    void openWindow() {
+        System.out.println("### BeforeEach");
+        Selenide.open("https://ya.ru");
+    }
+
+    @AfterEach
+    void close() {
+        System.out.println("### AfterEach");
+        Selenide.closeWindow();
+    }
+
+    @AfterAll
+    static void cleanDB() {
+        System.out.println("### AfterAll");
+    }
+
+    @Test
+    void assertTest1() {
+
+        //...1
+
+    }
+
+    @Test
+    void assertTest2() {
+
+        //...2
+
+    }
 }
